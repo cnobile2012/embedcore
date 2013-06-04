89c34bb4e52cc0826afb63aa95abfc359dbd915f
Carl J. Nobile <carl.nobile@gmail.com>
2013-06-04 07:58:54 -0400

    Added to the __init__.py file.

---
+++ interfaces/i2c/__init__.py
@@ -0,0 +1,14 @@
+#
+# interfaces/i2c/__init__.py
+#
+"""
+I2C code.
+
+by: Carl J. Nobile
+
+email: carl.nobile@gmail.com
+"""
+__docformat__ = "restructuredtext en"
+
+
+from .i2c import I2CException, I2C
